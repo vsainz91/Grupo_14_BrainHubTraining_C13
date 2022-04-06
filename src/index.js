4bf5c39fb63cdfc8491fae1214591a84479aa2ce
Jhon González <enriquemime@gmail.com>
2022-04-06 18:22:30 -0300

    agregando products list, creating and edit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require ('express'); 
 const app = express(); 
 const path = require ('path');
+const fs = require('fs');
 const PORT = 3050
 
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+/* enrutadores */
+const productsRouter = require ('./routes/productsRouter');
 
 /* Views config */
 app.set('view engine', 'ejs')
@@ -25,6 +30,13 @@ app.get('/productDetail', function(req, res) {
 app.get('/register', function(req, res) {
     res.sendFile(path.join(__dirname, '../views/register.html')) 
 } )
+
+module.exports = {
+    getProducts: JSON.parse(fs.readFileSync(path.join(__dirname, "products.json"), "utf-8")),
+    writeProducts: (data) => {
+        fs.writeFileSync(path.join(__dirname, "products.json"), JSON.stringify(data));
+    }
+}
 
 app.listen(PORT, () => console.log(`
 Servidor escuchando en el puerto ${PORT}
