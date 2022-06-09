bf7c49aae09490ddbbb3481c494a2fd3703d1dfa
Valeria Sainz <vsainz1@hotmail.com>
2022-06-09 17:53:48 -0300

    Actualiza script de db

--- src/database/brainhubScript.sql
+++ src/database/brainhubScript.sql
@@ -6,9 +6,14 @@
 CREATE TABLE `categories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `course_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--  Auto-generated SQL script #202206091730
+INSERT INTO brainhub_db.categories (id,name)
+	VALUES (1,'Idiomas');
+INSERT INTO brainhub_db.categories (id,name)
+	VALUES (2,'Programacion');
 
 
 -- brainhub_db.course_images definition
@@ -16,8 +21,23 @@ CREATE TABLE `categories` (
 CREATE TABLE `course_images` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `image_name` varchar(100) NOT NULL,
+  `course_id` int(10) NOT null,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--  Auto-generated SQL script #202206091725
+INSERT INTO brainhub_db.courses_images (id,image_name,course_id)
+	VALUES (1,'curso-english1.jpg',1);
+INSERT INTO brainhub_db.courses_images (id,image_name,course_id)
+	VALUES (2,'curso-portugues.jpg',2);
+INSERT INTO brainhub_db.courses_images (id,image_name,course_id)
+	VALUES (3,'curso-chino1.jpg',3);
+INSERT INTO brainhub_db.courses_images (id,image_name,course_id)
+	VALUES (4,'curso-reactjs1.png',4);
+INSERT INTO brainhub_db.courses_images (id,image_name,course_id)
+	VALUES (5,'curso-DataScience.jpg',5);
+INSERT INTO brainhub_db.courses_images (id,image_name,course_id)
+	VALUES (6,'curso-Nodejs.jpg',6);
 
 
 -- brainhub_db.courses definition
@@ -31,9 +51,29 @@ CREATE TABLE `courses` (
   `practice_time` int(10) unsigned NOT NULL,
   `lessons` int(10) unsigned NOT NULL,
   `content_hours` int(11) NOT NULL,
+  `courses_images_id` int(10) null,
   `category_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO brainhub_db.courses
+(id, name, price, description, instructor, practice_time, lessons, content_hours, category_id, courses_images_id)
+VALUES(1, 'Curso Completo de Inglés: Iniciación', 3500, 'Curso completo de inglés para dominar la gramática, traducción, conversación, vocabulario, pronunciación y mucho más.', 'Mark Thompson', 16, 28, 2, 1, NULL);
+INSERT INTO brainhub_db.courses
+(id, name, price, description, instructor, practice_time, lessons, content_hours, category_id, courses_images_id)
+VALUES(2, 'Portugués de Brasil básico', 4050, 'Este curso está diseñado para hispanohablantes y enseña de forma objetiva, con explicaciones claras y concisas, el idioma portugués', 'Joao Fernandez', 25, 73, 4, 1, NULL);
+INSERT INTO brainhub_db.courses
+(id, name, price, description, instructor, practice_time, lessons, content_hours, category_id, courses_images_id)
+VALUES(3, 'Aprender Chino a la primera', 5500, 'Curso de chino. Empieza desde cero y habla desde el primer vídeo de la forma más dinámica y divertida.', 'Ariel Lin', 41, 210, 14, 1, NULL);
+INSERT INTO brainhub_db.courses
+(id, name, price, description, instructor, practice_time, lessons, content_hours, category_id, courses_images_id)
+VALUES(4, 'Curso de Introducción a React.js', 4500, 'Trabaja con componentes, propiedades, estado y efectos. Almacena tu información en Local Storage, implementa React Context para comunicar componentes y teletransporta componentes con React Portals. Crea tu primer proyecto web con React junto a tu profesor.', 'Pepita Perez', 14, 21, 5, 2, NULL);
+INSERT INTO brainhub_db.courses
+(id, name, price, description, instructor, practice_time, lessons, content_hours, category_id, courses_images_id)
+VALUES(5, 'Cómo y Por Qué Aprender Data Science e Inteligencia Artificial', 5000, 'Conoce cómo iniciar una carrera en data science e inteligencia artificial entendiendo cómo funciona esta área de la tecnología, qué oportunidades tiene y qué caminos seguir para conseguir tu primer trabajo en datos.', 'Mariano Rodriguez', 12, 22, 2, 2, NULL);
+INSERT INTO brainhub_db.courses
+(id, name, price, description, instructor, practice_time, lessons, content_hours, category_id, courses_images_id)
+VALUES(6, 'Curso de Backend con Node.js: API REST con Express.js', 5300, 'Trabaja con rutas, servidores y middlewares de Express.js. Construye una API, manipula errores y haz validación de datos. Despliega tu aplicación a producción en Heroku.', 'Juan García', 12, 25, 3, 2, NULL);
 
 
 -- brainhub_db.users definition
@@ -54,6 +94,14 @@ CREATE TABLE `users` (
 
 CREATE TABLE `users_rol` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rol_name` tinyint(4) NOT NULL,
+  `rol_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--  Auto-generated SQL script #202206091748
+INSERT INTO brainhub_db.users_rol (id,rol_name)
+	VALUES (1,'USER');
+INSERT INTO brainhub_db.users_rol (id,rol_name)
+	VALUES (2,'ADMIN');
+
+
